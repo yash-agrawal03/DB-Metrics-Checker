3b5f58654df6b9a7089d1a78554236d430204245
Yash Agrawal <yash.agrawal1@flipkart.com>
2026-01-06 16:30:55 +0530

    setup files

--- db-metrics-api/src/main/java/com/dbmetrics/api/DbMetricsApiApplication.java
+++ db-metrics-api/src/main/java/com/dbmetrics/api/DbMetricsApiApplication.java
@@ -1,5 +1,6 @@
 package com.dbmetrics.api;
 
+import hypertest.javaagent.HypertestAgent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,8 +18,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class DbMetricsApiApplication {
     
     public static void main(String[] args) {
+        String serviceIdentifier = "5c4be515-f188-49b7-a1e7-1c1f5c72174f";
+        String exporterUrl = "https://logger.v3-app.hypertest.co";
+        HypertestAgent.start(serviceIdentifier, "fk-db-metrics-checker", "API-KEY", exporterUrl, DbMetricsApiApplication.class);
         SpringApplication.run(DbMetricsApiApplication.class, args);
+
     }
     
 }
+
 
